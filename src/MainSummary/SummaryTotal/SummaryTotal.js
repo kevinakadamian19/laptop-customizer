96eeb95378fa4e7dec2a23d26c07301c58906e80
Kevin Damian Ta <kevindamianta@gmail.com>
2019-08-26 02:01:05 -0700

    SummaryTotal
    This component adds all the item in the cart to display the total purchase value of the user's custom laptop.

---
+++ src/MainSummary/SummaryTotal/SummaryTotal.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import './SummaryTotal.css'
+
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+class SummaryTotal extends Component {
+    render() {
+        const total = Object.keys(this.props.currentSelected.selected).reduce(
+            (acc, curr) => acc + this.props.currentSelected.selected[curr].cost,
+            0
+        );
+        return(
+            <div className="summary__total">
+                <div className="summary__total__label">Total</div>
+                <div className="summary__total__value">
+                        {USCurrencyFormat.format(total)}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SummaryTotal
